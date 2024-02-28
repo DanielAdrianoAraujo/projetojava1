d8791ac66059236cd59aece2499cae02d9dc2b13
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-02-28 19:46:47 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main metodo auto excutavel em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+	}
+
+}
