4d2ca2c696ac28e722ae8bf2053bf76aad129373
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-03-25 19:12:12 -0300

    Thread em objeto e o Runnable

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -6,41 +6,46 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
-			public void run() {
-				for (int pos = 0; pos < 5; pos++) {
-					System.out.println("Testando Thread");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			};
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-		}.start(); // Start liga a thread e fica processando
+		Thread threadNf = new Thread(thread2);
+		threadNf.start();
 
-		
-		
-//		****************Thread 2*****************************************************
-		
-		new Thread() {
-			public void run() {
-				for (int pos = 0; pos < 5; pos++) {
-					System.out.println("Testando Thread 2");
-					try {
-						Thread.sleep(3000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			};
-
-		}.start(); // Start liga a thread e fica processando
-
-		
 		System.out.println("Chegou ao Fim");
 		JOptionPane.showMessageDialog(null, "Sistema Continua Executando");
 	}
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 5; pos++) {
+				System.out.println("Testando Thread 2");
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			for (int pos = 0; pos < 5; pos++) {
+				System.out.println("Testando Thread");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
 
 }
