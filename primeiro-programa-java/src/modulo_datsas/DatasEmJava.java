aab6f1d49db53d3f52d1588944bf810dc77331a0
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-04-01 18:39:02 -0300

    Conhecendo o objeto Calendar

--- primeiro-programa-java/src/modulo_datsas/DatasEmJava.java
+++ primeiro-programa-java/src/modulo_datsas/DatasEmJava.java
@@ -2,27 +2,45 @@ package modulo_datsas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
 	
 	public static void main(String[] args) throws ParseException {
 		
+		Calendar calendar = Calendar.getInstance();
+		
 		Date date =  new Date();
 		
 		System.out.println("Data Milisegundos: " + date.getTime());
 		
+		System.out.println("Caledar Data Milisegundos: " + 
+		calendar.getTimeInMillis());
+		
 		System.out.println("Dia do Mes: " + date.getDate());
+		System.out.println("Calendar Dia do Mes: " + 
+		calendar.get(Calendar.DAY_OF_MONTH));
 		
 		System.out.println("Dia da Semana: " + date.getDay());
+		System.out.println("Calendar Dia da Semana: " + 
+		(calendar.get(Calendar.DAY_OF_WEEK)- 1));
 		
 		System.out.println("Hora do Dia: " + date.getHours());
+		System.out.println("Calendar Hora do Dia: " + 
+		calendar.get(Calendar.HOUR_OF_DAY));
 		
 		System.out.println("Minuto da Hora: " + date.getMinutes());
+		System.out.println("Calendar Minuto da Hora: " + 
+		calendar.get(Calendar.MINUTE));
 		
 		System.out.println("Segundos: " + date.getSeconds());
+		System.out.println("Calendar Segundos: " + 
+		calendar.get(Calendar.SECOND));
 		
 		System.out.println("Ano: " + (date.getYear() + 1900));
+		System.out.println("Calendar Ano: " + 
+		calendar.get(Calendar.YEAR));
 		
 		// ---------------- Simple Date Format ----------------------
 		
@@ -30,6 +48,10 @@ public class DatasEmJava {
 				new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
 		System.out.println("Data Atual: " + simpleDateFormat.format(date));
+		
+		System.out.println("Calendar Data Atual: " + 
+		simpleDateFormat.format(calendar.getInstance().getTime()));
+		
 		
 		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
