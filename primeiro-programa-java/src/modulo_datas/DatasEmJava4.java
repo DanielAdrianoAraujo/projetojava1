1f04552a5a97bea22223013dc6cef1dfab298d64
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-04-08 17:25:52 -0300

    Formatando data e hora com nova API - Java 8

---
+++ primeiro-programa-java/src/modulo_datas/DatasEmJava4.java
@@ -0,0 +1,32 @@
+package modulo_datas;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DatasEmJava4 {
+
+	public static void main(String[] args) {
+		 
+		// Nova API de data a partir do Java 8
+		
+		LocalDate dataAtual = LocalDate.now();
+		System.out.println("Data Atual: " + 
+		dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		LocalTime horaAtual = LocalTime.now();
+		System.out.println("Hora Atual: " + 
+		horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+		
+		LocalDateTime dataHoraAtual = LocalDateTime.now();
+		System.out.println("Data e Hora Atual / TimeStemp: " + 
+		dataHoraAtual.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+		
+		
+		
+		
+
+	}
+
+}
