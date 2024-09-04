ed3e610d58263f51fb737babe9149a85f8a0527f
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-09-04 11:23:42 -0300

    Criando o DAO e inserindo dados no BD com SQL

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,14 +2,27 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.jupiter.api.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
 	
 	@Test
 	public void initBancoJdbc(){
-		SingleConnection.getConnection();
+		//SingleConnection.getConnection();
+		
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userPosJava = new Userposjava();
+		
+		userPosJava.setId(4L);
+		userPosJava.setNome("Ben");
+		userPosJava.setEmail("danfreeflyer@yahoo.com.br");
+		
+		
+		userPosDAO.salvar(userPosJava);
+		
+		
 		
 	}
 
