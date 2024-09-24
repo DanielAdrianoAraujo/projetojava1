be7e53a870e8bbfe09dbfbe587621a1f51a81965
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-09-24 20:31:44 -0300

    Criando JPA com Hibernate, incluindo BD no PostGreSQL

---
+++ pos-java-maven-hibernate-agora/src/main/java/model/UsuarioPessoa.java
@@ -0,0 +1,69 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UsuarioPessoa {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	private String nome;
+	private String Sobrenome;
+	private String email;
+	private String login;
+	private String senha;
+	private int Idade;
+	
+	public void setIdade(int Idade) {
+		this.Idade = Idade;
+	}
+	
+	public int getIdade() {
+		return Idade;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
---
+++ pos-java-maven-hibernate-agora/src/test/java/posjavamavenhibernateagora/TesteHibernate.java
@@ -0,0 +1,14 @@
+package posjavamavenhibernateagora;
+
+import org.junit.jupiter.api.Test;
+
+public class TesteHibernate {
+	
+	@Test
+	public void testeHibernateutil() {
+		HibernateUtil.getEntityManager();
+		
+		
+	}
+	
+}
