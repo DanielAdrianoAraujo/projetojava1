f3b1135a607223eb7834ac8a3542cd4e54428048
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-10-26 16:40:55 -0300

    Spring Boot Criação EndPoint FindId, e Camada de Serviço

--- ProjetoTesteSpring/src/main/java/com/ProativaDigital/ProjetoTesteSpring/resources/UserResource.java
+++ ProjetoTesteSpring/src/main/java/com/ProativaDigital/ProjetoTesteSpring/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.ProativaDigital.ProjetoTesteSpring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ProativaDigital.ProjetoTesteSpring.entities.User;
+import com.ProativaDigital.ProjetoTesteSpring.services.UserService;
 
 
-@RestController // Recurso WEB implmentado por um controlador REST
+@RestController // Recurso WEB implmentado por um Controlador REST
 @RequestMapping(value = "/users") // Caminho do Recurso ou pagina WEB
 public class UserResource {
 	
-	@GetMapping // Responde o tipo GET do HTTP
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Daniel", "daniel@gmail.com", "998237489", "4332");
-		return ResponseEntity.ok().body(u);		
+	
+	@Autowired // Gestão de DP com o UserService
+	private UserService service;
+	
+	@GetMapping // Responde o tipo GET do HTTP => EndPoint
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);		
+	}
+	
+	@GetMapping(value = "/{id}") // EndPoint find by id
+	public ResponseEntity<User> finById(@PathVariable Long id) {
+		User obj = service.findById(id);	
+		return ResponseEntity.ok().body(obj);		
 	}
 	
 
---
+++ ProjetoTesteSpring/src/main/java/com/ProativaDigital/ProjetoTesteSpring/services/UserService.java
@@ -0,0 +1,28 @@
+package com.ProativaDigital.ProjetoTesteSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ProativaDigital.ProjetoTesteSpring.entities.User;
+import com.ProativaDigital.ProjetoTesteSpring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired // Gestão de Dependencia com o UserRepository
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
