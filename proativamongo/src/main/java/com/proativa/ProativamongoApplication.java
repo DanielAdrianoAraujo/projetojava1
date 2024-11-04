901281313dcac3882b980481c6ed4c62ac907ebe
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-11-04 10:21:15 -0300

    Spring Boot com MongoDB Inicio do Projeto

---
+++ proativamongo/src/main/java/com/proativa/ProativamongoApplication.java
@@ -0,0 +1,13 @@
+package com.proativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProativamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProativamongoApplication.class, args);
+	}
+
+}
---
+++ proativamongo/src/test/java/com/proativa/ProativamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProativamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
