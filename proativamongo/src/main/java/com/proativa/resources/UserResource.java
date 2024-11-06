82e70c351ca678503bef9c49baa56a12f08c6d51
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-11-06 10:31:45 -0300

    Spring Boot MongoDB Insert de dados com retorno de URL com novo código.

--- proativamongo/src/main/java/com/proativa/resources/UserResource.java
+++ proativamongo/src/main/java/com/proativa/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.proativa.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.proativa.domain.User;
 import com.proativa.dto.UserDTO;
@@ -34,4 +38,14 @@ public class UserResource {
 		User obj = service.findById(id);	
 		return ResponseEntity.ok().body(new UserDTO(obj));		
 	}
+	
+	@PostMapping // EndPoint para insert
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDto(objDto);	
+		obj = service.insert(obj);
+		// Após a inserção dos dados no MongoDB ele retorna a URL com o novo código inserido
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();		
+	}
+	
 }
