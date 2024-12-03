c2e31afda1d964f2cb96e2f3676eb26022be4ffe
Daniel Adriano de Araujo <danfreeflyer@yahoo.com.br>
2024-12-03 12:21:54 -0300

    Spring Boot Criando entity Post, dados do author DTO e ref do usuário

--- proativamongo/src/main/java/com/proativa/config/Instantiation.java
+++ proativamongo/src/main/java/com/proativa/config/Instantiation.java
@@ -1,36 +1,56 @@
 package com.proativa.config;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.proativa.domain.Post;
 import com.proativa.domain.User;
+import com.proativa.dto.AuthorDTO;
+import com.proativa.repository.PostRepository;
 import com.proativa.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
-	
 	@Autowired
 	private UserRepository userRepository;
-	
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
-		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
-		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
-		
+		postRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("17/03/2018"), "Partiu Viagem", "Vou viajar para Orlando, abraços",
+				new AuthorDTO(maria));
+
+		Post post2 = new Post(null, sdf.parse("20/03/2018"), "Bom dia", "Vamos aos Parques", 
+				new AuthorDTO(maria));
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
+
 	}
-	
-	
 
 }
---
+++ proativamongo/src/main/java/com/proativa/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.proativa.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proativa.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+}
